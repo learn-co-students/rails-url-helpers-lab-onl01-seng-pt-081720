f5402ac5eb90558420c93e88d23bf1594a097fbd
Janae Quinones <janaemassa@gmail.com>
2021-01-17 17:11:59 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'pry', '~> 0.13.1'
 
 group :development, :test do
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Janae', last_name: 'Quinones')
+Student.create(first_name: 'Andy', last_name: 'Manchego')
